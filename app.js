c1c49bab9701d601df7a6d88ecc8eafbb54a058e
Andy Mukherjee <andy.mukherjee2005@gmail.com>
2015-04-27 15:40:15 -0400

    test

--- app.js
+++ app.js
@@ -98,54 +98,21 @@ var getUnanswered = function(tags) {
 //-------------------------------------------------------------------
 // this function takes the question object returned by StackOverflow 
 // and creates new result to be appended to DOM
-var showAnswere = function(answere) {
+var showUser = function(user) {
 	
 	// clone our result template code
-	var result = $('.templates .Answere').clone();
+	var result = $('.templates .user').clone();
 	
 	// Set the question properties in result
-	var qElem = result.find('.q-text a');
-	qElem.attr('href', q.link);
-	qElem.text(q.title);
+	var userElem = result.find('.user-name a');
+	userElem.attr('href', user.user.link);
+	userElem.text(user.user.display_name);
 	//set the answere properties in result
-	var answereElem = result.find('.answere-text a');
-	answereElem.attr('href', answere.link);
-	answereElem.text(answere.title);
+	var reputationElem = result.find('.reputation a');
+	reputationElem.attr('href', user.user.link);
+	reputationElem.text(user.user.reputation);
 
-	// set the date asked property in result
-	var asked = result.find('.asked-date');
-	var date = new Date(1000*question.creation_date);
-	asked.text(date.toString());
 
-	// set the #views for question property in result
-	var viewed = result.find('.viewed');
-	viewed.text(question.view_count);
-
-	// set some properties related to asker
-	var asker = result.find('.asker');
-	asker.html('<p>Name: <a target="_blank" href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
-													question.owner.display_name +
-												'</a>' +
-							'</p>' +
- 							'<p>Reputation: ' + question.owner.reputation + '</p>'
-	);
-
-	
-// set the date replied property in result
-	var replied = result.find('.replied-date');
-	var date = new Date(1000*answere.replied_date);
-	replied.text(date.toString());
-
-	
-
-	// set some properties related to the replier
-	var replier = result.find('.replier');
-	replier.html('<p>Name: <a target="_blank" href=http://stackoverflow.com/users/' + answere.owner.user_id + ' >' +
-													answere.owner.display_name +
-												'</a>' +
-							'</p>' +
- 							'<p>Reputation: ' + answere.owner.reputation + '</p>'
-	);
 
 	return result;
 };
@@ -173,7 +140,7 @@ var getInspiration = function(tags) {
 	var request = {
 								site: 'stackoverflow',
 								};
-	
+	var s= tags;
 	var result = $.ajax({
 		url: "http://api.stackexchange.com/2.2/tags/"+tags+"/top-answerers/all_time",
 		data: request,
@@ -181,13 +148,13 @@ var getInspiration = function(tags) {
 		type: "GET",
 		})
 	.done(function(result){
-		var searchResults = showSearchResults(request, result.items.length);
+		var searchResults = showSearchResults(s, result.items.length);
 
 		$('.search-results').html(searchResults);
 
 		$.each(result.items, function(i, item) {
-			var question = showQuestion(item);
-			$('.results').append(Answere);
+			var user = showUser(item);
+			$('.results').append(user);
 		});
 	})
 	.fail(function(jqXHR, error, errorThrown){
